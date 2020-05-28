4feabe1fef9b2007312ff5c72e72dc6a455d853e
caicindy87 <caicindy87@gmail.com>
2020-05-28 15:39:38 -0700

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,32 @@
-// add code snippets from README
+let state;
+
+function reducer(state = { count: 0 }, action) {
+  switch (action.type) {
+    case "INCREASE_COUNT":
+      return { count: state.count + 1 };
+    default:
+      return state;
+  }
+}
+
+function dispatch(action) {
+  state = reducer(state, action);
+  render();
+}
+
+function render() {
+  const container = document.getElementById("container");
+  container.textContent = state.count;
+}
+
+dispatch({ type: "@@INIT" });
+
+function listenToButtonClick() {
+  let button = document.getElementById("button");
+
+  button.addEventListener("click", () => {
+    dispatch({ type: "INCREASE_COUNT" });
+  });
+}
+
+listenToButtonClick();
